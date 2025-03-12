07dd6a8640fe6fe2dc938c0c9bf4063b3dc73791
Leslie Kogi <lesliekogi@gmail.com>
2025-03-12 21:55:59 +0300

    Arithmetic questions

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+/* multiply */
+const num1 = 31
+const num2 = 2
+let multiply=(num1*num2)
 
-// Write your code here
+console.log(multiply)
+
+/* random */
+
+
+const random=Math.ceil(Math.random())
+
+console.log(random)
+
+/* mod */
+
+let num3 =24
+let num4 =5
+let mod=(num3%=num4)
+
+console.log(mod)
+
+/*max*/
+
+const max= Math.max(1,7,17,20)
+
+console.log (max)
